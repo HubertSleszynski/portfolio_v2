21693526a7e2657afb60fb4069f4673bb0dd97b6
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-06-30 14:31:59 +0200

    updated about me section

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -8,12 +8,12 @@ export default function Info() {
       <div className="about__box">
         <SlBadge />
         <h3 className="about__title">Completed</h3>
-        <span className="about__subtitle">28+ Projects</span>
+        <span className="about__subtitle">35+ Projects</span>
       </div>
       <div className="about__box">
         <CgWorkAlt />
         <h3 className="about__title">Experience</h3>
-        <span className="about__subtitle">6+ Months</span>
+        <span className="about__subtitle">1 Year</span>
       </div>
       <div className="about__box">
         <IoSchoolOutline />
