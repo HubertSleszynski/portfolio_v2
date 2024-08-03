61d3eb390b76cf2a8b4642a4c55acd601667642b
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-08-03 11:43:00 +0200

    updated categories

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -13,7 +13,7 @@ export const projectsData = [
     technologies: "React | Next.js | TailwindCSS | Framer Motion",
     description:
       "A sleek and modern website developed for W'food, an Asian cuisine restaurant, utilizing Next.js for optimal performance and Tailwind CSS for a responsive, elegant design. Framer Motion was integrated to add smooth, interactive animations, creating a single-landing page that effectively showcases the restaurant's offerings and encourages reservations. ",
-    category: "Next",
+    category: "Full-Stack",
     live: "https://hrs-wfood2024.netlify.app/",
     github: "https://github.com/HubertSleszynski/restaurant-website",
   },
@@ -24,7 +24,7 @@ export const projectsData = [
     technologies: "Typescript | React | Next.js | TailwindCSS | Framer Motion",
     description:
       "Explore this gym website project, showcasing sections on the mission, available classes, trainer team, membership pricing, testimonials, and blog. This single-page application provides a comprehensive look at gym offerings and user experiences in a clean, intuitive interface.",
-    category: "Next",
+    category: "Full-Stack",
     live: "https://hrs-fitphysique.netlify.app/",
     github: "https://github.com/HubertSleszynski/fit-physique",
   },
@@ -35,7 +35,7 @@ export const projectsData = [
     technologies: "React | Next.js | TailwindCSS | Framer Motion",
     description:
       "My personal web project is a modern beauty salon website featuring Home page with a YouTube video modal, About section for service descriptions, Treatments part for detailing procedures, and Contact with salon info and an inquiry form. It showcases my frontend skills and user-centered design for the beauty industry",
-    category: "Next",
+    category: "Full-Stack",
     live: "https://hrs-serene.netlify.app/",
     github: "https://github.com/HubertSleszynski/serene",
   },
@@ -46,7 +46,7 @@ export const projectsData = [
     technologies: "React | Next.js | TailwindCSS | Framer Motion | GSAP ",
     description:
       "This web application offers a captivating presentation of a coffee shop using React, Next.js, TailwindCSS, Framer Motion and GSAP. It features eight sections, including video-enhanced Hero content, an in-depth Menu, and customer Testimonials, all designed to provide a seamless and engaging user experience.",
-    category: "React",
+    category: "Full-Stack",
     live: "https://hrs-zenbrew.netlify.app/",
     github: "https://github.com/HubertSleszynski/zenbrew",
   },
@@ -57,7 +57,7 @@ export const projectsData = [
     technologies: "React | TailwindCSS ",
     description:
       "Furry Trimz is a project born out of a passion for pet care. This application is a single-page-application designed to allow users to book grooming services at a professional pet salon. The project was built using React and Tailwind, providing both functionality and aesthetic design.",
-    category: "React",
+    category: "Frontend",
     live: "https://hrs-furry-trimz.netlify.app/",
     github: "https://github.com/HubertSleszynski/furry-trimz",
   },
@@ -79,10 +79,10 @@ export const projectsNav = [
     name: "All",
   },
   {
-    name: "React",
+    name: "Frontend",
   },
   {
-    name: "Next",
+    name: "Full-Stack",
   },
   {
     name: "Logic",
