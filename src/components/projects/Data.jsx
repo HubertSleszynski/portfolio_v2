3cd8c46795ac470cf97e20d45e7553f9784a3d91
Hubert Sleszynski <h.r.sleszynski@gmail.com>
2024-08-12 12:29:24 +0200

    updated categories

--- src/components/projects/Data.jsx
+++ src/components/projects/Data.jsx
@@ -10,10 +10,10 @@ export const projectsData = [
     id: 1,
     image: WFood,
     title: "W'Food",
-    technologies: "React | Next.js | TailwindCSS | Framer Motion",
+    technologies: "Next.js | TailwindCSS | Framer Motion",
     description:
       "A sleek and modern website developed for W'food, an Asian cuisine restaurant, utilizing Next.js for optimal performance and Tailwind CSS for a responsive, elegant design. Framer Motion was integrated to add smooth, interactive animations, creating a single-landing page that effectively showcases the restaurant's offerings and encourages reservations. ",
-    category: "Full-Stack",
+    category: "Next",
     live: "https://hrs-wfood2024.netlify.app/",
     github: "https://github.com/HubertSleszynski/restaurant-website",
   },
@@ -21,10 +21,10 @@ export const projectsData = [
     id: 2,
     image: FitPhysique,
     title: "FitPhysique",
-    technologies: "Typescript | React | Next.js | TailwindCSS | Framer Motion",
+    technologies: "Typescript | Next.js | TailwindCSS | Framer Motion",
     description:
       "Explore this gym website project, showcasing sections on the mission, available classes, trainer team, membership pricing, testimonials, and blog. This single-page application provides a comprehensive look at gym offerings and user experiences in a clean, intuitive interface.",
-    category: "Full-Stack",
+    category: "Next",
     live: "https://hrs-fitphysique.netlify.app/",
     github: "https://github.com/HubertSleszynski/fit-physique",
   },
@@ -32,10 +32,10 @@ export const projectsData = [
     id: 3,
     image: Serene,
     title: "Serene",
-    technologies: "React | Next.js | TailwindCSS | Framer Motion",
+    technologies: "Next.js | TailwindCSS | Framer Motion",
     description:
       "My personal web project is a modern beauty salon website featuring Home page with a YouTube video modal, About section for service descriptions, Treatments part for detailing procedures, and Contact with salon info and an inquiry form. It showcases my frontend skills and user-centered design for the beauty industry",
-    category: "Full-Stack",
+    category: "Next",
     live: "https://hrs-serene.netlify.app/",
     github: "https://github.com/HubertSleszynski/serene",
   },
@@ -43,10 +43,10 @@ export const projectsData = [
     id: 4,
     image: Zenbrew,
     title: "Zenbrew",
-    technologies: "React | Next.js | TailwindCSS | Framer Motion | GSAP ",
+    technologies: "Next.js | TailwindCSS | Framer Motion | GSAP ",
     description:
       "This web application offers a captivating presentation of a coffee shop using React, Next.js, TailwindCSS, Framer Motion and GSAP. It features eight sections, including video-enhanced Hero content, an in-depth Menu, and customer Testimonials, all designed to provide a seamless and engaging user experience.",
-    category: "Full-Stack",
+    category: "Next",
     live: "https://hrs-zenbrew.netlify.app/",
     github: "https://github.com/HubertSleszynski/zenbrew",
   },
@@ -57,7 +57,7 @@ export const projectsData = [
     technologies: "React | TailwindCSS ",
     description:
       "Furry Trimz is a project born out of a passion for pet care. This application is a single-page-application designed to allow users to book grooming services at a professional pet salon. The project was built using React and Tailwind, providing both functionality and aesthetic design.",
-    category: "Frontend",
+    category: "React",
     live: "https://hrs-furry-trimz.netlify.app/",
     github: "https://github.com/HubertSleszynski/furry-trimz",
   },
@@ -68,7 +68,7 @@ export const projectsData = [
     technologies: "React | SCSS",
     description:
       "Dancing knight is a type of mathematical puzzle involving moving a knight around a chessboard in such a way that it visits every square exactly once. The knight's movement is restricted - it moves two squares in one direction and one square in another direction, forming an 'L' shape.",
-    category: "Logic",
+    category: "React",
     live: "https://hrs-dancing-knight.netlify.app/",
     github: "https://github.com/HubertSleszynski/Dancing-Knight",
   },
@@ -79,12 +79,9 @@ export const projectsNav = [
     name: "All",
   },
   {
-    name: "Frontend",
+    name: "React",
   },
   {
-    name: "Full-Stack",
-  },
-  {
-    name: "Logic",
+    name: "Next",
   },
 ];
